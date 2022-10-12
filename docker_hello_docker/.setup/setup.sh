d965277bbc51da4886141eb3abd2a494d18e3ec7
Hubert Ströbitzer <hubstr@gmail.com>
2022-10-12 09:05:49 +0200

    fix setup and teardown

--- docker_hello_docker/.setup/setup.sh
+++ docker_hello_docker/.setup/setup.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-if docker ps --format '{{.Names}}' | grep -w my-nginx &> /dev/null; then
+if docker ps -a --format '{{.Names}}' | grep -w my-nginx &> /dev/null; then
     docker rm -f my-nginx
 fi
 
